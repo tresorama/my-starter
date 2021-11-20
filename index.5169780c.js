02c2cf20a7ed7811b0b45c7ca7a0313055d795c6
Jacopo Marrone <jacopo.marrone27@gmail.com>
2021-11-20 02:38:53 +0100

    Updates

---
+++ index.5169780c.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){(n=>{const e=document.createElement("h2");e.innerHTML=n,document.body.append(e)})("Javascript Working !!!")}));
+//# sourceMappingURL=index.5169780c.js.map
